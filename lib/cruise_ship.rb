2a9f29a2ed163d970a4eaae1979a7d1444a07375
Blaire <grant.blaire@gmail.com>
2020-11-21 14:10:41 -0600

    commit tree 46d75b6f52a388fd0d6e775447306341113ece13

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -5,7 +5,17 @@
 # suite_d: "Charlie Chaplin", 
 # suite_e: "Crumpet the Elf"
 # }
+#select_winner
+#returns the name of the passenger who stays in suite a and whose name begins with the letter 'A' 
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    return winner
+    end
+  end
 end
+
+
